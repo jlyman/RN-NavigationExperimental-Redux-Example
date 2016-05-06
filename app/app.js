a350beaff2947ea4d996a1660c0407b2f6e10c20
Farid from Safi <farid.lazreq@gmail.com>
2016-05-06 14:22:36 +0200

    add support for remote-redux-devtools and store refactoring

--- app/app.js
+++ app/app.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react'
-import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
-
-import reducers from './reducers'
 import AppContainer from './containers/AppContainer'
 
-const createStoreWithMiddleware = applyMiddleware(thunk)(createStore)
-const store = createStoreWithMiddleware(reducers)
+import configureStore from './store'
+const store = configureStore()
 
 export default class App extends Component {
 	render() {
